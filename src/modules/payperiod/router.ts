265e2847f43cb847098332ebe8c45324c60cf51c
bw47139 <bw47139@gmail.com>
2025-12-23 19:44:55 -0500

    Fix payperiod service casing for Linux

--- src/modules/payperiod/router.ts
+++ src/modules/payperiod/router.ts
@@ -1,4 +1,4 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
 import { verifyToken } from "../../middleware/verifyToken";
 import payPeriodRoutes from "./payPeriod.routes";
 
